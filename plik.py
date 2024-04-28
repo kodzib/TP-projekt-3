fa5f75051c504799240ccfd1445f9c24d14838cc
kodzib <kbrowq@gmail.com>
2024-04-28 22:28:50 +0200

    pruba dodania plotowania

--- plik.py
+++ plik.py
@@ -1,2 +1,10 @@
-from vsstudio.Debug.pybind11module import *
-say_hello()
+from vsstudio.Debug.pybind11module import * #nasza napisana biblioteka
+import soundfile as sf # wsparcie do wgrywanie audio
+import numpy as np
+
+say_hello() #sprawdzamy czy nasza biblioteka dziala
+
+test_audio_path = 'C:/Users/kbrow/OneDrive/Desktop/projekty/TP projekt 3/TP-projekt-3/test_audio_file_1.wav'
+
+audio_data, audio_samplerate = sf.read(test_audio_path)
+plot_audio(audio_data)
--- source/module/module.cpp
+++ source/module/module.cpp
@@ -1,13 +1,28 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 #include <stdio.h>
 #include <matplot/matplot.h>
+#include <cmath>
+
+
+namespace py = pybind11;
 
 void say_hello() {
 	printf("Hello World!\n");
+}
+
+void plot_audio(py::array_t<float> dane_to_plot) {
+	py::buffer_info array_buf = dane_to_plot.request();
+	float* ptr = static_cast<float*>(array_buf.ptr);
+	int size = array_buf.shape[0];
+	matplot::fplot([](double x) {return sin(x);}, std::array<double, 2>{-10, 10}, "x--b")->color("red").line_width(2);
+	matplot::save("C:/Users/kbrow/OneDrive/Desktop/projekty/TP projekt 3/TP-projekt-3/test_graph.jpg");
 }
 
 PYBIND11_MODULE(pybind11module, module) {
 	module.doc() = "Pybind11Module";
 
 	module.def("say_hello", &say_hello);
+	module.def("plot_audio", &plot_audio);
 }
