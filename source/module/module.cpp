431e1e166f5d67cec876ba2eee7c891e56fa6a1b
kodzib <kbrowq@gmail.com>
2024-05-02 14:49:04 +0200

    znalazlem blad w starym gaussie, dodanie poczatku innych filtrow

--- source/module/module.cpp
+++ source/module/module.cpp
@@ -81,14 +81,32 @@ py::array_t<double> filtracja_d(py::array_t<double> buf, const char type, const 
 	std::vector<double> dane(ptr, ptr + size);
 	std::vector<double> kernel(kernel_size);
 	double sum = 0.;
-	for (int i = 0; i < kernel_size; i++) {
-		double sigma = 1;
-		kernel[i] = (1 / sqrt(2 * M_PI * pow(sigma, 2))) * exp(-1 * pow(i, 2) / (2 * pow(sigma, 2)));
-		sum += kernel[i];
+	switch (type) {
+	case 'g': case 'G':
+		for (int i = 0; i < kernel_size; i++) {
+			double sigma = 1;
+			kernel[i] = (1 / sqrt(2 * M_PI * pow(sigma, 2))) * exp(-1 * pow(i, 2) / (2 * pow(sigma, 2)));
+			if (i == 0) {
+				sum += kernel[i];
+			}
+			else sum += 2*kernel[i];
+		}
+		break;
+	default:
+		for (int i = 0; i < kernel_size; i++) {
+			kernel[i] = 1;
+			if (i == 0) {
+				sum += kernel[i];
+			}
+			else sum += 2 * kernel[i];
+		}
+		break;
 	}
 	for (int i = 0; i < kernel_size; i++) {
 		kernel[i] /= sum;
+		std::cout << kernel[i] << std::endl;
 	}
+	
 	for (int i = 0; i < size; i++) {
 		double new_kernl_ammount = 0.;
 		for (int k = -1*(kernel_size - 1); k < kernel_size - 1; k++) {
